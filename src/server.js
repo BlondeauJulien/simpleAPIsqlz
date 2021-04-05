d68966dd0fb874b3b2abc7f62331720e781f7152
Julien Blondeau <julienblondeau@live.fr>
2021-04-05 16:47:55 +0200

    set up server

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import fs from 'fs';
+import path from 'path';
+//import routes from '/routes';
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true, limit: '50mb'}));
+
+app.use((req, res) => {
+  res.status(404).send('404: PAge not found');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port : ${process.env.PORT}`)
+})
